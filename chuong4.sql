cdab0e7f924c97828329730f53571b2efa976ea5
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-12-28 18:24:49 +0700

    Create chuong4.sql
    csdl gặp sự cố

---
+++ chuong4.sql
@@ -0,0 +1,10 @@
+Chương 4: Bảo mật và an toàn dữ liệu
+
+1. CSDL có thể gặp 1 số sự cố:
+- Đĩa chứa Data File hay Transaction Log File bị mất, bị hư hỏng.
+- Server bị hỏng.
+- Những thảm họa tự nhiên.
+- Các thiết bị dùng để Backup - Restore bị đánh cắp hay bị hư hỏng.
+- Lỗi user vô ý xóa, thao tác sai làm hư csdl.
+- Hành vi cố ý đưa thông tin sai lạc.
+- Bị hack.
